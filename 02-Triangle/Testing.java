b159ab0feee20c459722763b9223fb96cb907652
Sheikh Alif <salif30@stuy.edu>
2021-10-25 11:44:03 -0400

    end of class period

---
+++ 02-Triangle/Testing.java
@@ -0,0 +1,12 @@
+public class Testing{
+  public static void main(String[]args){
+    Point P1 = new Point(1,1);
+    Point P2 = new Point(2,2);
+    Point P3 = new Point(0, 0);
+    Point P4 = new Point(3, 4);
+    System.out.println(P1.toString());
+    System.out.println(P2.toString());
+    System.out.println(P3.toString());
+    System.out.println(P4.toString());
+  }
+}
---
+++ 02-Triangle/Triangle.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+
+public class Triangle{
+  private Point x, y, z;
+  public Triangle(Point a, Point b, Point c){
+    x = a;
+    y = b;
+    z = c;
+  }
+
+  public Triangle(double a, double b, double c, double d, double e, double f){
+    x = new Point(a, b);
+    y = new Point(c, d);
+    z = new Point(e, f);
+  }
+
+  public double getPerimeter(){
+    return 
+  }
+}
