f93cf51caacd41b6bff855cdc35df0433f78ff66
Sheikh Alif <salif30@stuy.edu>
2021-10-25 20:56:13 -0400

    almost done

--- 02-Triangle/Testing.java
+++ 02-Triangle/Testing.java
@@ -4,9 +4,25 @@ public class Testing{
     Point P2 = new Point(2,2);
     Point P3 = new Point(0, 0);
     Point P4 = new Point(3, 4);
+    Triangle T1 = new Triangle(P1, P2, P3);
+    Triangle T2 = new Triangle(P2, P3, P4);
+    Triangle T3 = new Triangle(1, 1, 2, 2, 0, 0);
+    Triangle T4 = new Triangle(2, 2, 0, 0, 3, 4);
     System.out.println(P1.toString());
     System.out.println(P2.toString());
     System.out.println(P3.toString());
     System.out.println(P4.toString());
+    System.out.println(T1.getPerimeter());
+    System.out.println(T2.getPerimeter());
+    System.out.println(T3.getPerimeter());
+    System.out.println(T4.getPerimeter());
+    System.out.println(T1.getVertex(1)); //should return (1, 1)
+    System.out.println(T1.getVertex(2)); //should return (2, 2)
+    System.out.println(T1.getVertex(3)); //should return (0, 0)
+    T3.setVertex(3, P4);
+    System.out.println("\n");
+    System.out.println(T3.getVertex(1));
+    System.out.println(T3.getVertex(2));
+    System.out.println(T3.getVertex(3)); //should return (3, 4);
   }
 }
--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -8,13 +8,28 @@ public class Triangle{
     z = c;
   }
 
-  public Triangle(double a, double b, double c, double d, double e, double f){
-    x = new Point(a, b);
-    y = new Point(c, d);
-    z = new Point(e, f);
+  public Triangle(double d, double e, double f, double g, double h, double i){
+    x = new Point(d, e);
+    y = new Point(f, g);
+    z = new Point(h, i);
   }
 
   public double getPerimeter(){
-    return 
+    return Math.sqrt(Math.pow(x.getX()-y.getX(), 2) + Math.pow(x.getY()-y.getY(), 2)) + Math.sqrt(Math.pow(x.getX()-z.getX(), 2) + Math.pow(x.getY()-z.getY(), 2)) + Math.sqrt(Math.pow(y.getX()-z.getX(), 2) + Math.pow(y.getY()-z.getY(), 2));
+  }
+
+  public Point getVertex(int index){
+    Point[] arr = {x, y, z};
+    return arr[index - 1];
+  }
+
+  public void setVertex(int index, Point change){
+    if (index == 1)x = new Point(change.getX(), change.getY());
+    if (index == 2)y = new Point(change.getX(), change.getY());
+    if (index == 3)z = new Point(change.getX(), change.getY());
+  }
+
+  public String toString(){
+    return ("{"+ x +", " + y + ", " + z + "}");
   }
 }
