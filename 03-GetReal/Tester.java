afb8f3b6a819408308772b80c25e08d4896bdbde
Sheikh Alif <salif30@stuy.edu>
2021-11-01 11:52:18 -0400

    end of class period

---
+++ 03-GetReal/Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String args[]){
+    RationalNumber example1 = new RationalNumber(4, 7);
+    RealNumber example2 = new RealNumber(6.8);
+    System.out.println(example2.add(example1));
+  }
+}
