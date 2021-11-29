dc2a0c9def628881699be71e64fbf9dcd815974a
Sheikh Alif <salif30@stuy.edu>
2021-11-29 11:40:59 -0500

    fixed triangles name

---
+++ 06-Advent/Triangles.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Triangle {
+  public static void main(String[] args) {
+    int mode = Integer.parseInt(args[1]);
+    String fileName = args[0];
+    int total = 0;
+    try {
+      File file = new File(fileName);
+      Scanner input = new Scanner(file);
+      if (mode == 1){
+        while (input.hasNextInt()) {
+          total += validTri(input.nextInt(), input.nextInt(), input.nextInt());
+        }
+      }
+      if (mode == 2){
+        while (input.hasNextInt()){
+          int a1 = input.nextInt();
+          int a2 = input.nextInt();
+          int a3 = input.nextInt();
+          int b1 = input.nextInt();
+          int b2 = input.nextInt();
+          int b3 = input.nextInt();
+          int c1 = input.nextInt();
+          int c2 = input.nextInt();
+          int c3 = input.nextInt();
+          total += validTri(a1, b1, c1);
+          total += validTri(a2, b2, c2);
+          total += validTri(a3, b3, c3);
+        }
+      }
+      input.close();//releases the file from your program
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("Not a valid file name");
+    }
+    System.out.println(total);
+  }
+
+  public static int validTri(int a, int b, int c){
+    if (a + b <= c){
+      return 0;
+    }
+    if (b + c <= a){
+      return 0;
+    }
+    if (a + c <= b){
+      return 0;
+    }
+    return 1;
+  }
+}
