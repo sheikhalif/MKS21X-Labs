3cd9d595fa8ad0c5dba38bc0c2dbc14c77ab5548
Sheikh Alif <salif30@stuy.edu>
2021-11-30 11:42:25 -0500

    end of class period

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.lang.Math;
-public class Wrapping {
+public class Non {
   public static void main(String[] args) {
     String fileName = args[0];
     int total = 0;
@@ -17,5 +17,23 @@ public class Wrapping {
     System.out.println(total);
   }
 
-  public static int
+  public static boolean part1a(String s){
+    int vowels = 0;
+    for (int i = 0; i<s.length(); i++){
+      char letter = s.charAt(i);
+      if (letter.equals('a') || letter.equals('e') || letter.equals('i') || letter.equals('o') || letter.equals('u')){
+        vowels++;
+      }
+      if (vowels ==  3){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean part1b(String s){
+    for (int i = 0; i<s.length()-1; i++){
+      String section = s.substring(i, i+2);
+    }
+  }
 }
