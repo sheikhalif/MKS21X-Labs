9e04f68c87b9640c22d051f47161a9dbfb5cefd9
Sheikh Alif <salif30@stuy.edu>
2022-01-08 23:38:17 -0500

    added attack and specialattack methods to wizard. will start testing

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -16,6 +16,28 @@ public class Wizard extends Adventurer {
     setMana(mana);
   }
 
+  public void attack(Damageable other){
+      int damage = (int)(Math.random()*10)+1;
+      other.applyDamage(damage);
+      setMana(getMana() + 1);
+      System.out.println(this +
+          " attacked " + other + " for " +
+          damage + " damage!");
+  }
+
+  public void specialAttack(Damageable other){
+     if(getMana() >= 10){
+          int damage = (int)(Math.random()*20)+1;
+          other.applyDamage(damage);
+          System.out.println(this + " cast a spell on "
+           + other + " for " + damage + " damage! "+ spellName);
+          setMana(getMana() - 10);
+    }else{
+        System.out.println("Not enough mana! ");
+        attack(other);
+    }
+  }
+
   public int getMana(){
      return mana;
   }
