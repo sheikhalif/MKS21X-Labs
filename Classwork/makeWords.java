49a1b93efa2d3ded8f74f0b4da9d677d7ed73a17
Sheikh Alif <salif30@stuy.edu>
2022-02-02 09:59:03 -0500

    end of class period

---
+++ Classwork/makeWords.java
@@ -0,0 +1,10 @@
+public class makeWords{
+  public static void makeWords(int size, String ans){
+    for (int i = 0; i < size; i++){
+      for (char c = 'a'; x <= 'z'; x++){
+
+      }
+    }
+    System.out.println(ans);
+  }
+}
